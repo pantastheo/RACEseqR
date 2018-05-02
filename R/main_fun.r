114e7719490940e29d1720a6ab3da9d059a44166
Pantazis Theotokis <pan.theo@hotmail.com>
2018-05-02 02:16:19 +0100

    added delete file function
    moved the delete files to a seperate function

--- R/main_fun.r
+++ R/main_fun.r
@@ -55,7 +55,7 @@ if(!is.na(str) & !is.na(end)) {
       stop("More than one reference file")
     } else if ((length(refname))==1) {
       replicon_ref<- as.character(refname)
-    } 
+    }
 
   #input the data in .fastq or .fastq.gz format
   data_fastq<- list.files(".", pattern="fastq", all.files = F, full.names = F)
@@ -65,7 +65,7 @@ if(!is.na(str) & !is.na(end)) {
       stop("More than one .fastq file")
     } else if ((length(data_fastq))==1) {
       input_data<- data_fastq
-    } 
+    }
 }else {stop("Please input Start and End nucleotide positions \n Or type [option] -h for help")}
 
 #reading and transforming reference sequence
@@ -77,7 +77,7 @@ filename <- paste("mm", mismatch, sep = "")
 out_name <- paste("read_count_", filename, sep="")
 
 if (tmap_opt==TRUE){
-  
+
   prefix<-"tmap"
 
   #build the index
@@ -86,36 +86,36 @@ if (tmap_opt==TRUE){
 
   #perform alignment with tmap and read count using bedtools
   if (is.na(RACE_adapter)){
-    
+
     #no adapter trimming
     print(paste0("Performing alignment with ", mismatch, " mismatch using tmap"))
     CMD_tmap<- paste("tmap map1 -a 0 -g 3 --max-mismatches ",mismatch," -f ", replicon_ref," -r ", input_data, " | samtools view -bt ", replicon_ref," - | genomeCoverageBed -d -5 -ibam stdin > ",out_name, sep="")
   system(CMD_tmap)
   } else {
-    
+
     #adapter trimming using cutadapt
     print(paste0("Performing adapter trimming and alignment with ", mismatch, " mismatch using tmap"))
     CMD_tmap<- paste("cutadapt -g ", RACE_adapter, " -e0 --no-indels -m10 --discard-untrimmed --quiet ", input_data," |tmap map1 -a 0 -g 3 --max-mismatches ",mismatch," -f ", replicon_ref," -i fastq | samtools view -bt ", replicon_ref," - | genomeCoverageBed -d -5 -ibam stdin > ",out_name, sep="")
-    system(CMD_tmap) 
+    system(CMD_tmap)
   }
 } else {
-  
+
   prefix<-"bowtie"
-   
-  #build the index 
+
+  #build the index
   CMD_bowindex<- paste("bowtie-build -q -f", replicon_ref, "index", sep=" ")
   system(CMD_bowindex)
 
   #perform alignment with bowtie and read count using bedtools
   if (is.na(RACE_adapter)){
-    
+
     #no adapter trimming
     print(paste0("Performing alignment with ", mismatch, " mismatch using bowtie"))
     CMD_bow<- paste("bowtie -p 2 -S -k 1 -v", mismatch, "index", input_data," | samtools view -bS - | genomeCoverageBed -d -5 -ibam stdin >", out_name, sep=" ")
   system(CMD_bow)
 
   } else {
-    
+
     #adapter trimming using cutadapt
     print(paste0("Performing adapter trimming and alignment with ", mismatch, " mismatch using bowtie"))
     CMD_bow<- paste("cutadapt -g", RACE_adapter, "-e0 --no-indels -m10 --discard-untrimmed --quiet ", input_data,"|bowtie -p 8 -S -k 1 -v", mismatch, "index - | samtools view -bS - | genomeCoverageBed -d -5 -ibam stdin >", out_name, sep=" ")
@@ -126,7 +126,7 @@ if (tmap_opt==TRUE){
 #read and merge ref and reads
 reads<- read.delim(out_name, header = F )
 dataframe<- data.frame(reads, nt_reference , stringsAsFactors = F)
-    
+
 #calculating the % and log10 columns
 dataframe[,5] <- (dataframe[,3]/sum(dataframe[,3])*100)
 dataframe[,6] <- (log10(dataframe[,3]))
@@ -136,6 +136,26 @@ dataframe[dataframe== -Inf] <-0
 binding_region <- dataframe[str:end,]
 
 #remove read_count and index file created
+
+
+
+#' File delete function
+#'
+#' Function to delete files generated in the process
+#' @param pattern The filename extension to be deleted
+#' @keywords pattern
+#' @export
+#' @examples
+#' del_files()
+
+del_files<- function(pattern){
+  fl_rm<-list.files(".", pattern = pattern, all.files = F, full.names = F)
+  for(i in fl_rm){
+    i<-paste("rm", i , sep = " ")
+    system(i)
+  }
+}
+
 counts<- list.files(".", pattern="read_count", all.files = F, full.names = F)
 for(i in counts ){
   i<- paste("rm", i , sep=" ")
@@ -164,6 +184,27 @@ for(i in index_files ){
 
 return(binding_region)
 }
+
+#remove read_count and index file created
+
+#' File delete function
+#'
+#' Function to delete files generated in the process
+#' @param pattern The filename extension to be deleted
+#' @keywords pattern
+#' @export
+#' @examples
+#' del_files()
+
+del_files<- function(pattern){
+  fl_rm<-list.files(".", pattern = pattern, all.files = F, full.names = F)
+  for(i in fl_rm){
+    i<-paste("rm", i , sep = " ")
+    system(i)
+  }
+}
+
+
 
 #' RACEseq dataframe function
 #'
@@ -196,33 +237,33 @@ out_plot<- function(binding_region, filename){
 
   #create wildtype linear & log scale graph
   pdf(paste0(filename, ".pdf"), width=15)
-    
+
   mp <- barplot(binding_region[,5],
                 ylab="Novel 5\' Ends (%)",
-                xlab="Binding site", 
-                names.arg=(binding_region[,4]), 
-                las=1, 
-                cex.names = 2.2, 
-                col="darkgrey" , 
+                xlab="Binding site",
+                names.arg=(binding_region[,4]),
+                las=1,
+                cex.names = 2.2,
+                col="darkgrey" ,
                 main="Novel 5' Ends in linear",
                 cex.main=2.3,
                 cex.lab=1.3,
                 ylim=c(0,100))
   text(mp,binding_region[,5]+5 ,cex = 1.3, adj = 0 ,labels=binding_region[,3] ,srt=90)
-  
+
   #in log10  logarithmic scale
   mp <- barplot(binding_region[,6],
                 ylab=expression("Novel 5\' Ends (log"[10]*")"),
-                xlab="Binding site", 
-                names.arg=(binding_region[,4]), 
-                las=1, 
-                cex.names = 2.2, 
-                col="darkgrey", 
+                xlab="Binding site",
+                names.arg=(binding_region[,4]),
+                las=1,
+                cex.names = 2.2,
+                col="darkgrey",
                 main="Novel 5' Ends in logarithmic",
                 cex.main=2.3,
                 cex.lab=1.3,
                 ylim=c(0,10))
   text(mp,binding_region[,6]+0.5 ,cex = 1.3, adj = 0 ,labels=binding_region[,3] ,srt=90)
-  # dev.off()
-  
+  dev.off()
+
 }
